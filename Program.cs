884bd365077d347263a4ae47014eb2133f81f76d
Bryan Gutierrez Polanco <BryanGP93@gmail.com>
2019-09-30 17:54:53 -0400

    Cargando

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perimetro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Cuadrado, Perimetro;
+            Console.WriteLine("Escribe el numero de cuadrado");
+            Cuadrado = int.Parse(Console.ReadLine());
+            Perimetro = Cuadrado * 4;
+            Console.WriteLine("El perimetro del cuadrado {0} es {1}",Cuadrado,Perimetro);
+            Console.ReadKey();
+
+        }
+    }
+}
